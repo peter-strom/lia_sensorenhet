240b3bd6f8fb7d29cb57b6c04c79bea9fd027e1f
peter <peter@kungariket.se>
2023-04-03 15:48:20 +0200

    implemented heater function

--- Inc/sht40.h
+++ Inc/sht40.h
@@ -1,5 +1,8 @@
 /**
- * SHT40 temp and humidity sensor
+ * @brief STH40 Humidity and Temperature Sensor
+ * https://www.mouser.se/datasheet/2/682/Datasheet_SHT4x-3003109.pdf
+ * @todo CRC check (not highest priority)
+ *
  */
 #ifndef __SHT40_H__
 #define __SHT40_H__
@@ -9,17 +12,19 @@
 #define SHT40_SERIAL_NUMBER 0x89
 #define SHT40_HIGH_PRECISION_200MW_1S 0x39
 #define SHT40_HIGH_PRECISION_200MW_100MS 0x32
+#define SHT40_HIGH_PRECISION_110MW_1S 0x2F
 
 #include "main.h"
 #include "i2c.h"
+#include <stdbool.h>
 typedef struct SHT40
 {
-    uint8_t temperatureC;
-    uint8_t humidityRH;
+  uint8_t temperatureC;
+  uint8_t humidityRH;
 } SHT40;
 
 SHT40 new_SHT40(void);
-void SHT40_read_high_precision(SHT40 *self);
+bool SHT40_read_high_precision(SHT40 *self, uint8_t heaterMode);
 void SHT40_print(SHT40 *self);
 
 #endif /*__SHT40_H__ */
--- Src/sht40.c
+++ Src/sht40.c
@@ -1,49 +1,82 @@
 #include "sht40.h"
 
-static void SHT40_read_from_sensor(SHT40 *self, uint8_t dev_addr, uint8_t command);
+static bool SHT40_read_from_sensor(SHT40 *self, uint8_t devAddr, uint8_t command, uint16_t delayMs);
 
 SHT40 new_SHT40()
 {
-    SHT40 self;
-    self.humidityRH = 0;
-    self.temperatureC = 0;
-    return self;
+  SHT40 self;
+  self.humidityRH = 0;
+  self.temperatureC = 0;
+  return self;
 }
 
-void SHT40_read_high_precision(SHT40 *self)
+/**
+ * @brief read sensor values and store tha data into a struct.
+ * Note: the heater affect the temperature.
+ * heater mode 0 = off
+ * heater mode 1 = 200 mW for 1 s
+ * heater mode 2 = 200 mW for 100 ms
+ * heater mode 3 = 110 mW for 1 s
+ * @param[out] self the struct to store data
+ * @param[in] heaterMode operates the build in heater to remove condensed water 
+ * @return true if successfull
+ */
+bool SHT40_read_high_precision(SHT40 *self, uint8_t heaterMode)
 {
-    SHT40_read_from_sensor(self, SHT40_DEV_ADDR, SHT40_HIGH_PRECISION);
+  bool statusOk = true;
+  switch (heaterMode)
+  {
+  case 1:
+    statusOk &= SHT40_read_from_sensor(self, SHT40_DEV_ADDR, SHT40_HIGH_PRECISION_200MW_1S, 1010);
+    break;
+
+  case 2:
+    statusOk &= SHT40_read_from_sensor(self, SHT40_DEV_ADDR, SHT40_HIGH_PRECISION_200MW_100MS, 110);
+    break;
+
+  case 3:
+    statusOk &= SHT40_read_from_sensor(self, SHT40_DEV_ADDR, SHT40_HIGH_PRECISION_110MW_1S, 1010);
+    break;
+
+  default:
+    statusOk &= SHT40_read_from_sensor(self, SHT40_DEV_ADDR, SHT40_HIGH_PRECISION, 10);
+  }
+  return statusOk;
 }
 
-void SHT40_read_from_sensor(SHT40 *self, uint8_t dev_addr, uint8_t command)
+/**
+ * @brief sends command and reads the result from the sensor
+ * 
+ * @param[out] self the struct to store data
+ * @param[in] devAddr device 12c adress
+ * @param[in] command sensor command to execute
+ * @param[in] delayMs delay needed before reading the result
+ * @return true if successfull
+ */
+static bool SHT40_read_from_sensor(SHT40 *self, uint8_t devAddr, uint8_t command, uint16_t delayMs)
 {
-    if (HAL_I2C_IsDeviceReady(&hi2c1, dev_addr, 1, 100) != HAL_OK)
-    {
+  bool statusOk = true;
+  uint8_t rxBuff[6];
+  uint8_t txBuff[1] = {command};
+  statusOk &= HAL_I2C_Master_Transmit(&hi2c1, devAddr, txBuff, 1, 100);
+  HAL_Delay(delayMs);
+  statusOk &= HAL_I2C_Master_Receive(&hi2c1, devAddr, rxBuff, 6, 100);
 #ifdef DEBUG_MODE
-        printf("error connecting to device address: %#04x\n\r", dev_addr >> 1);
+  printf("temp_data0: %#06x temp_data1: %#06x temp_checksum: %#06x rh_data0: %#06x rh_data1: %#06x rh_checksum: %#06x\n\r",
+         rxBuff[0], rxBuff[1], rxBuff[2], rxBuff[3], rxBuff[4], rxBuff[5]);
 #endif
-    }
-    else
-    {
-#ifdef DEBUG_MODE
-        printf("connecting to device address: %#04x\n\r", dev_addr >> 1);
-#endif
-        uint8_t rx_buff[6];
-        uint8_t tx_buff[1] = {command};
-        HAL_I2C_Master_Transmit(&hi2c1, dev_addr, tx_buff, 1, 100);
-        HAL_Delay(10);
-        HAL_I2C_Master_Receive(&hi2c1, dev_addr, rx_buff, 6, 100);
-#ifdef DEBUG_MODE
-        printf("temp_data0: %#06x temp_data1: %#06x temp_checksum: %#06x rh_data0: %#06x rh_data1: %#06x rh_checksum: %#06x\n\r",
-               rx_buff[0], rx_buff[1], rx_buff[2], rx_buff[3], rx_buff[4], rx_buff[5]);
-#endif
-        self->temperatureC = 175 * (rx_buff[0] << 8 | rx_buff[1]) / 65535 - 45;
-        self->humidityRH = 125 * (rx_buff[3] << 8 | rx_buff[4]) / 65535 - 6;
-    }
+  self->temperatureC = 175 * (rxBuff[0] << 8 | rxBuff[1]) / 65535 - 45;
+  self->humidityRH = 125 * (rxBuff[3] << 8 | rxBuff[4]) / 65535 - 6;
+  return statusOk;
 }
 
+/**
+ * @brief prints the data from the struct
+ * 
+ * @param[in] self the struct that stores the data
+ */
 void SHT40_print(SHT40 *self)
 {
-    printf("temp: %d degrees C \n\r", self->temperatureC);
-    printf("humidity: %d RH \n\r", self->humidityRH);
+  printf("temp: %d degrees C \n\r", self->temperatureC);
+  printf("humidity: %d RH \n\r", self->humidityRH);
 }
